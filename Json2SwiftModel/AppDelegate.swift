c39da100e52c02804a2d44990b7cae37db21c6a0
ke he <smileshitou@hotmail.com>
2016-04-14 21:39:51 +0800

    Initial Commit

---
+++ Json2SwiftModel/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Json2SwiftModel
+//
+//  Created by heke on 16/4/14.
+//  Copyright © 2016年 mhk. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Json2SwiftModel/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Json2SwiftModel
+//
+//  Created by heke on 16/4/14.
+//  Copyright © 2016年 mhk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
